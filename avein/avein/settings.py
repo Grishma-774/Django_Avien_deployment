b007173f2486860963e42b940771c370561088d1
Grishma-774 <gellaboinagrishma@gmail.com>
2026-01-17 12:12:21 +0530

    host_added

--- avein/avein/settings.py
+++ avein/avein/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["django-avien-deployment.onrender.com"]
 
 
 # Application definition
